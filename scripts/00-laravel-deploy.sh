03c9bb4b78194091d77d8035e1554d0097e0db15
olacodeX <161823813+DPHPGuy@users.noreply.github.com>
2024-04-17 08:58:52 +0100

    optimize command for deployment

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -6,11 +6,11 @@ composer install --no-dev --working-dir=/var/www/html
 # echo "generating application key..."
 # php artisan key:generate --show
 
-echo "Caching config..."
-php artisan config:cache
+# echo "Caching config..."
+php artisan optimize:clear
 
-echo "Caching routes..."
-php artisan route:cache
+# echo "Caching routes..."
+# php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
